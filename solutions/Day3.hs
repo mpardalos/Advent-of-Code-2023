ed82758006f38db683f2d8799c7f6d5900b4a35b
Michalis Pardalos <me@mpardalos.com>
2023-12-03 13:43:35 +0000

    Set up day 3

---
+++ solutions/Day3.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day3 (part1, part2) where
+
+import Data.ByteString (ByteString)
+
+part1 :: ByteString -> Int
+part1 input = 0
+
+part2 :: ByteString -> Int
+part2 input = 0
--- solutions/Solutions.hs
+++ solutions/Solutions.hs
@@ -6,6 +6,7 @@ import Control.DeepSeq (NFData)
 import Data.ByteString (ByteString)
 import Day1 (part1, part2)
 import Day2 (part1, part2)
+import Day3 (part1, part2)
 
 data Solution where
   MkSolution :: (NFData a, DisplaySolution a) => String -> (ByteString -> a) -> FilePath -> Solution
@@ -27,5 +28,7 @@ solutions =
   [ MkSolution "Day 1 part 1" Day1.part1 "day1",
     MkSolution "Day 1 part 2" Day1.part2 "day1",
     MkSolution "Day 2 part 1" Day2.part1 "day2",
-    MkSolution "Day 2 part 2" Day2.part2 "day2"
+    MkSolution "Day 2 part 2" Day2.part2 "day2",
+    MkSolution "Day 3 part 1" Day3.part1 "day3",
+    MkSolution "Day 3 part 2" Day3.part2 "day3"
   ]
