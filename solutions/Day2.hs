b0665892c5f1d59875f54bbbcf4127fb75801bdd
Michalis Pardalos <me@mpardalos.com>
2023-12-03 12:01:22 +0000

    Set up day 2

---
+++ solutions/Day2.hs
@@ -0,0 +1,9 @@
+module Day2 (part1, part2) where
+
+import Data.ByteString (ByteString)
+
+part1 :: ByteString -> Int
+part1 = const 0
+
+part2 :: ByteString -> Int
+part2 = const 0
--- solutions/Solutions.hs
+++ solutions/Solutions.hs
@@ -5,6 +5,7 @@ module Solutions (DisplaySolution (..), Solution (..), solutions) where
 import Control.DeepSeq (NFData)
 import Data.ByteString (ByteString)
 import Day1 (part1, part2)
+import Day2 (part1, part2)
 
 data Solution where
   MkSolution :: (NFData a, DisplaySolution a) => String -> (ByteString -> a) -> FilePath -> Solution
@@ -24,5 +25,7 @@ instance DisplaySolution String where
 solutions :: [Solution]
 solutions =
   [ MkSolution "Day 1 part 1" Day1.part1 "day1",
-    MkSolution "Day 1 part 2" Day1.part2 "day1"
+    MkSolution "Day 1 part 2" Day1.part2 "day1",
+    MkSolution "Day 2 part 1" Day2.part1 "day2",
+    MkSolution "Day 2 part 2" Day2.part2 "day2"
   ]
