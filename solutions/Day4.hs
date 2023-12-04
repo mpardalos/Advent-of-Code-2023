31997bf1182582218bf6fbc3b05f0898d29a3069
Michalis Pardalos <me@mpardalos.com>
2023-12-04 11:12:48 +0000

    Set up day 4

---
+++ solutions/Day4.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Day4 (part1, part2) where
+
+import Data.ByteString (ByteString)
+
+part1 :: ByteString -> Int
+part1 input = 0
+
+part2 :: ByteString -> Int
+part2 input = 0
--- solutions/Solutions.hs
+++ solutions/Solutions.hs
@@ -7,6 +7,7 @@ import Data.ByteString (ByteString)
 import Day1 (part1, part2)
 import Day2 (part1, part2)
 import Day3 (part1, part2)
+import Day4 (part1, part2)
 
 data Solution where
   MkSolution :: (NFData a, DisplaySolution a) => String -> (ByteString -> a) -> FilePath -> Solution
@@ -30,5 +31,7 @@ solutions =
     MkSolution "Day 2 part 1" Day2.part1 "day2",
     MkSolution "Day 2 part 2" Day2.part2 "day2",
     MkSolution "Day 3 part 1" Day3.part1 "day3",
-    MkSolution "Day 3 part 2" Day3.part2 "day3"
+    MkSolution "Day 3 part 2" Day3.part2 "day3",
+    MkSolution "Day 4 part 1" Day4.part1 "day4",
+    MkSolution "Day 4 part 2" Day4.part2 "day4"
   ]
