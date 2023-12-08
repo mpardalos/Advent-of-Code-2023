45e2a821e994d4d37bc310b8ebc845ca69049172
Michalis Pardalos <me@mpardalos.com>
2023-12-08 10:34:18 +0000

    Set up day 8

--- solutions/Solutions.hs
+++ solutions/Solutions.hs
@@ -13,6 +13,7 @@ import Day4 (part1, part2)
 import Day5 (part1, part2)
 import Day6 (part1, part2)
 import Day7 (part1, part2)
+import Day8 (part1, part2)
 
 data Solution where
   MkSolution :: (NFData a, DisplaySolution a) => String -> (ByteString -> a) -> FilePath -> Solution
@@ -49,5 +50,7 @@ solutions =
     MkSolution "Day 6 part 1" Day6.part1 "day6",
     MkSolution "Day 6 part 2" Day6.part2 "day6",
     MkSolution "Day 7 part 1" Day7.part1 "day7",
-    MkSolution "Day 7 part 2" Day7.part2 "day7"
+    MkSolution "Day 7 part 2" Day7.part2 "day7",
+    MkSolution "Day 8 part 1" Day8.part1 "day8",
+    MkSolution "Day 8 part 2" Day8.part2 "day8"
   ]
