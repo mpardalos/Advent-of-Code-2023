96dca11c9e82249cd03d66b1c59f4d622bc01017
Michalis Pardalos <me@mpardalos.com>
2023-12-10 16:22:33 +0000

    Only traverse one direction for day 10

--- solutions/Day10.hs
+++ solutions/Day10.hs
@@ -10,47 +10,54 @@ import Data.Map.Strict qualified as Map
 
 type Position = (Int, Int)
 
-type Direction = (Int, Int)
-
 type Pipe = Char
 
 type Grid a = Map Position a
 
 part1 :: ByteString -> Int
-part1 input =
-  let goingOneWay = go 1 startPos start1
-      goingTheOtherWay = go 1 startPos start2
-   in -- in grid `seq`
-      --      (goingOneWay `par` goingTheOtherWay) `seq`
-      --        (maximum (Map.unionWith min goingOneWay goingTheOtherWay))
-      maximum (Map.unionWith min goingOneWay goingTheOtherWay)
+part1 input = length (traverseFromTo grid startPos startDir) `div` 2
   where
     grid = parse input
+    startPos = findStartPos grid
+    startDir = head $ directionsFrom grid startPos
 
-    go :: Int -> Position -> Position -> Grid Int
-    go _ _ pos | Map.notMember pos grid = Map.empty
-    go d prevPos@(prevRow, prevCol) pos@(row, col) = case grid Map.! pos of
-      'S' -> Map.empty
-      '.' -> Map.empty
-      -- row - prevRow should be +1 or -1, and indicates the direction we are
-      -- travelling, so we can keep going the same way
-      '|' -> Map.insert pos d $ go (d + 1) pos $ (row + (row - prevRow), col)
-      '-' -> Map.insert pos d $ go (d + 1) pos $ (row, col + (col - prevCol))
-      -- We also need to keep travelling the same direction here, but the logic
-      -- is a bit more complex because of the corner
-      'L' -> Map.insert pos d $ go (d + 1) pos $ if prevPos == up then right else up
-      'J' -> Map.insert pos d $ go (d + 1) pos $ if prevPos == up then left else up
-      '7' -> Map.insert pos d $ go (d + 1) pos $ if prevPos == down then left else down
-      'F' -> Map.insert pos d $ go (d + 1) pos $ if prevPos == down then right else down
-      c -> error ("Invalid character: " ++ show c)
-      where
-        up = (row - 1, col)
-        right = (row, col + 1)
-        down = (row + 1, col)
-        left = (row, col - 1)
+part2 :: ByteString -> ()
+part2 _ = ()
 
-    startPos@(startRow, startCol) = Map.foldlWithKey (\acc pos c -> if c == 'S' then pos else acc) (-1, -1) grid
+findStartPos :: Grid Pipe -> Position
+findStartPos = Map.foldlWithKey (\acc pos c -> if c == 'S' then pos else acc) (-1, -1)
 
+traverseFromTo :: Grid Pipe -> Position -> Position -> [Position]
+traverseFromTo grid _ pos | Map.notMember pos grid = []
+traverseFromTo grid prevPos@(prevRow, prevCol) pos@(row, col) = case grid Map.! pos of
+  'S' -> [pos]
+  '.' -> []
+  -- row - prevRow should be +1 or -1, and indicates the direction we are
+  -- travelling, so we can keep going the same way
+  '|' -> pos : (traverseFromTo grid pos $ (row + (row - prevRow), col))
+  '-' -> pos : (traverseFromTo grid pos $ (row, col + (col - prevCol)))
+  -- We also need to keep travelling the same direction here, but the logic
+  -- is a bit more complex because of the corner
+  'L' -> pos : (traverseFromTo grid pos $ if prevPos == up then right else up)
+  'J' -> pos : (traverseFromTo grid pos $ if prevPos == up then left else up)
+  '7' -> pos : (traverseFromTo grid pos $ if prevPos == down then left else down)
+  'F' -> pos : (traverseFromTo grid pos $ if prevPos == down then right else down)
+  c -> error ("Invalid character: " ++ show c)
+  where
+    up = (row - 1, col)
+    right = (row, col + 1)
+    down = (row + 1, col)
+    left = (row, col - 1)
+
+directionsFrom :: Grid Pipe -> Position -> [Position]
+directionsFrom grid (startRow, startCol) =
+  map snd . filter fst $
+    [ (startUp, (startRow - 1, startCol)),
+      (startRight, (startRow, startCol + 1)),
+      (startDown, (startRow + 1, startCol)),
+      (startLeft, (startRow, startCol - 1))
+    ]
+  where
     startUp = case Map.lookup (startRow - 1, startCol) grid of
       Nothing -> False
       Just c -> c `elem` "|7F"
@@ -66,18 +73,6 @@ part1 input =
     startLeft = case Map.lookup (startRow - 1, startCol) grid of
       Nothing -> False
       Just c -> c `elem` "-LF"
-
-    start1, start2 :: Direction
-    [(True, start1), (True, start2)] =
-      filter fst $
-        [ (startUp, (startRow - 1, startCol)),
-          (startRight, (startRow, startCol + 1)),
-          (startDown, (startRow + 1, startCol)),
-          (startLeft, (startRow, startCol - 1))
-        ]
-
-part2 :: ByteString -> ()
-part2 _ = ()
 
 parse :: ByteString -> Grid Pipe
 parse = Map.unions . parMap rseq (uncurry parseLine) . zip [0 ..] . BS.lines
