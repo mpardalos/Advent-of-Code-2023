e13ab08d2bbe84a31a700cfd5956ebcb2acfea0b
Michalis Pardalos <me@mpardalos.com>
2023-12-12 21:53:37 +0000

    Clean up traces and formatting

--- solutions/Day12.hs
+++ solutions/Day12.hs
@@ -5,13 +5,8 @@ module Day12 (part1, part2) where
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
 import Data.Function ((&))
--- import Debug.Trace (trace)
-import Text.Printf (printf)
-import Util (readIntsSepBy)
 import Data.List (intercalate)
-
-trace :: String -> a -> a
-trace _ x = x
+import Util (readIntsSepBy)
 
 part1 :: ByteString -> Int
 part1 = sum . map (uncurry countConfigurations) . parse
@@ -21,25 +16,28 @@ countConfigurations = go False
   where
     go :: Bool -> [Char] -> [Int] -> Int
     go inContiguous (char : chars) (count : counts) = case (count, char) of
-      (_, _) | trace (printf "inContiguous=%-5s | char=%c | count=%d | %s" (show inContiguous) char count (char : chars)) False -> undefined
-      (0, '#') -> trace "end=0 (too many #)" 0
+      (0, '#') -> 0
       (0, '.') -> go False chars counts
-      (0, '?') -> trace "is ." (go False chars counts)
-      (_, '.') -> if inContiguous then 0 else go False chars (count : counts)
+      (0, '?') -> go False chars counts
+      (_, '.') ->
+        if inContiguous
+          then 0
+          else go False chars (count : counts)
       (_, '#') -> go True chars (count - 1 : counts)
       (_, '?') ->
         if inContiguous
-          then trace "is #" (go True chars (count - 1 : counts))
-          else let
-                asDot = go False chars (count : counts)
+          then go True chars (count - 1 : counts)
+          else
+            let asDot = go False chars (count : counts)
                 asHash = go True chars (count - 1 : counts)
+             in asDot + asHash
       (_, _) -> error ("Invalid character: " ++ show char)
-    go _ [] [0] = trace "end=1 (end of input)" 1
-    go _ [] (_ : _) = trace "end=0 (end of input)" 0
+    go _ [] [0] = 1
+    go _ [] (_ : _) = 0
     go inContiguous chars [] =
       if not inContiguous && not (any (== '#') chars)
-        then trace "end=1" 1
-        else trace "end=0" 0
+        then 1
+        else 0
 
 part2 :: ByteString -> ()
 part2 _ = ()
