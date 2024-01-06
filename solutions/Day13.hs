82e35cd21162066b6caf09d1683f100dffb2d61b
Michalis Pardalos <me@mpardalos.com>
2024-01-06 23:03:07 +0000

    WIP day13

--- solutions/Day13.hs
+++ solutions/Day13.hs
@@ -1,9 +1,15 @@
 module Day13 (part1, part2) where
 
+import Data.Attoparsec.ByteString.Char8 (endOfLine, sepBy)
 import Data.ByteString (ByteString)
+import Data.SparseGrid (SparseGrid, sparseGridP)
+import Util (unsafeParse, (<<$>>))
 
 part1 :: ByteString -> ()
 part1 _ = ()
 
 part2 :: ByteString -> ()
 part2 _ = ()
+
+parse :: ByteString -> [SparseGrid ()]
+parse = unsafeParse ((const () <<$>> sparseGridP '.') `sepBy` endOfLine)
